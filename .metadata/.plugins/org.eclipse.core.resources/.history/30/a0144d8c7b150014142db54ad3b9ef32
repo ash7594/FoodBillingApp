5dd0eee32dbbd3ae68b78c02d05c82a5fb6b8b23
Ashutosh <ash7594@gmail.com>
2014-07-27 15:40:41 +0400

    custom adapter for sidemenu

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/30/a0144d8c7b150014142db54ad3b9ef32
@@ -0,0 +1,11 @@
+package com.example.foodbilling;
+
+import java.util.ArrayList;
+
+public class CustomViewGroups extends ArrayList<String>{
+	
+	public CustomViewGroups() {
+		super.
+	}
+
+}
--- FoodBilling/src/com/example/foodbilling/SidemenuFragment.java
+++ FoodBilling/src/com/example/foodbilling/SidemenuFragment.java
@@ -1,16 +1,28 @@
 package com.example.foodbilling;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class SidemenuFragment extends Fragment {
+public class SidemenuFragment extends ListFragment {
 	
-	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { 
-		
-		View view = inflater.inflate(R.layout.fragment_sidemenu, container, false); 
-		return view; 
-		}
-	}
+	@Override
+	  public void onActivityCreated(Bundle savedInstanceState) {
+	    super.onActivityCreated(savedInstanceState);
+	    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+	        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+	        "Linux", "OS/2" };
+	    CustomViewGroups adapter = new CustomViewGroups(getActivity(), values);
+	    setListAdapter(adapter);
+	  }
+
+	  @Override
+	  public void onListItemClick(ListView l, View v, int position, long id) {
+	    // do something with the data
+	  }
+}
